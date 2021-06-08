ba2003e121509e8aa407a8bec5637e5ae14725cb
Alexio Bec <alexio.bec@gmail.com>
2021-06-08 16:38:42 +0200

    j'ai changé le main.c

--- main.c
+++ main.c
@@ -3,10 +3,7 @@
 #include "date.h"
 
 void main( ) {
-    date * d = malloc( sizeof( date ) );
-    d->jour = 8;
-    d->mois = 6;
-    d->an = 2021;
+    create_date( 8 , 6 , 2021 );
     print_date( d );
 
 }
